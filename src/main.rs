203132ff52670162358367554ca2fd1b72aca004
Diana <5275194+DianaNites@users.noreply.github.com>
2020-03-26 18:26:54 -0400

    disable help subcommand

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,7 @@ enum Commands {
         AppSettings::GlobalVersion,
         AppSettings::DisableVersion,
         AppSettings::SubcommandsNegateReqs,
+        AppSettings::DisableHelpSubcommand,
     ]
 ))]
 struct Args {
