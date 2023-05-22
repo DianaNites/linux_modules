d39954070d2abc7985a9511791b0beeecca9b070
Diana <5275194+DianaNites@users.noreply.github.com>
2023-05-21 21:54:44 -0700

    Insta UI tests

---
+++ tests/ui.rs
@@ -0,0 +1,27 @@
+#![allow(unused_imports, unused_mut, unreachable_code)]
+use std::str::from_utf8;
+
+use anyhow::Result;
+use assert_cmd::{prelude::*, Command};
+use insta::assert_snapshot;
+
+/// amdgpu is a complicated module that makes use of many features,
+/// all of which we want to display correctly.
+///
+/// This test will inherently have unstable output, the goal is to make sure
+/// the formatting is correct, not the contents.
+// TODO: Figure out `insta` filters to ignore changing contents
+#[test]
+fn amdgpu_info() -> Result<()> {
+    let mut cmd = Command::cargo_bin("nms")?;
+    cmd //.
+        .env_clear()
+        .env("COLUMNS", "120")
+        .args(["info", "amdgpu"]);
+    let out = cmd.output()?;
+    let out = from_utf8(&out.stdout)?;
+
+    assert_snapshot!(out);
+
+    Ok(())
+}
